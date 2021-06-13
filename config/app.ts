a4cb6560ea4fb7e6167172df6855f88322e2e09b
Luiiiz <52682525+meLuiiiz@users.noreply.github.com>
2021-06-13 03:14:58 -0300

    Adicionando cores para sistema de temas

--- config/app.ts
+++ config/app.ts
@@ -1,8 +1,16 @@
-import { ConfigApp } from "../src/Interface/Config"
+import { ConfigApp, ConfigTheme } from "../src/Interface/Config"
 
 export const app: ConfigApp = {
     name: process.env.DISCORD_APP_NAME,
     prefix: process.env.DISCORD_APP_PREFIX
 }
 
-export const theme = {}
+export const theme: ConfigTheme = {
+    colors: {
+        default: '#202225',
+        success: '#57F287',
+        error: '#ED4245',
+        warn: '#FEE75C',
+        info: '#0284C7',
+    }
+}
